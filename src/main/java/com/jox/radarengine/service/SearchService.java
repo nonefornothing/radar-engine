930256e0906355020b45508cbb6104eb1fa6cfe3
C120121003 <johan.prianto@bankmandiri.co.id>
2021-08-18 15:33:21 +0700

    add search service design

---
+++ src/main/java/com/jox/radarengine/service/SearchService.java
@@ -0,0 +1,10 @@
+package com.jox.radarengine.service;
+
+public interface SearchService {
+
+    public String searchByNPWP(int id);
+
+    public String searchByName(String name);
+
+    public String searchBySIUP(String siup);
+}
